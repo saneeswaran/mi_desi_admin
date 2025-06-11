bc4f59686df9c10ed7106d6138be5f3760543312
Sathish <124010734+saneeswaran@users.noreply.github.com>
2025-06-11 05:51:00 -0700

    Update banner_model.dart

--- lib/model/banner_model.dart
+++ lib/model/banner_model.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, 
 class BannerModel {
   final String bannerId;
   final String imageUrl;
